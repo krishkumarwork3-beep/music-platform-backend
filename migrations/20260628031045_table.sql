c0071a7e821eebe63c244132b9d7da7c29347b61
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-29 06:11:58 +0530

    Create tracks table

--- migrations/20260628031045_table.sql
+++ migrations/20260628031045_table.sql
@@ -10,3 +10,16 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+-- Tracks Table
+CREATE TABLE tracks (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    title VARCHAR(255),
+    artist VARCHAR(255),
+    duration INTERVAL,
+    file_name TEXT,
+    upload_status VARCHAR(50) DEFAULT 'incomplete',
+    thumbnail_name TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
