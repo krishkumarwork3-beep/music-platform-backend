860ca66c4958fe96df1180b35af16ba6fa50896a
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-29 06:36:57 +0530

    Add duration model

--- src/models.rs
+++ src/models.rs
@@ -2,3 +2,9 @@ use serde::{Deserialize, Serialize};
 use sqlx::{postgres::types::PgInterval, FromRow};
 use uuid::Uuid;
 use chrono::NaiveDateTime;
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Duration {
+    pub months: i32,
+    pub days: i32,
+    pub microseconds: i64,
+}
